ec22146322e2b25013b4a887f9949755af0033c3
Federico Vacca <fede.mxpx@gmail.com>
2022-06-22 19:55:58 -0300

    entrega fetch

--- js/script.js
+++ js/script.js
@@ -34,13 +34,13 @@ function ingresarUsuario() {
 }
 
 
-function fSwitch(){
-    document.getElementById('formlogin').style.display='none';
-    document.getElementById('formsorteo').style.display= 'block';
+function fSwitch() {
+    document.getElementById('formlogin').style.display = 'none';
+    document.getElementById('formsorteo').style.display = 'block';
 
 
 
-    
+
 }
 
 function cuentaExistente() {
@@ -116,3 +116,74 @@ function sortear() {
         imageAlt: 'ganador',
     })
 }
+
+window.addEventListener('load', () => {
+    let lon
+    let lat
+
+    let tempValor = document.getElementById('valor');
+    let tempDescripcion = document.getElementById('descripcion');
+
+    let ubicacion = document.getElementById('ubicacion');
+    let icono = document.getElementById('icono');
+
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(posicion => {
+            lon = posicion.coords.longitude
+            lat = posicion.coords.latitude
+
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=43f5ecc10a16a7c83a7bf122da1dbb01`
+
+            fetch(url)
+                .then(Response => {
+                    return Response.json()
+                })
+                .then(data => {
+                    let temp = Math.round(data.main.temp)
+                    tempValor.textContent = `${temp} °C`
+                    let desc = data.weather[0].description
+                    tempDescripcion.textContent = desc.toUpperCase()
+                    ubicacion.textContent = data.name
+
+                    switch (data.weather[0].main) {
+                        case 'Thunderstorm':
+                            icono.src = 'img/thunder.svg'
+                            console.log('TORMENTA');
+                            break;
+                        case 'Drizzle':
+                            icono.src = 'img/rainy-2.svg'
+                            console.log('LLOVIZNA');
+                            break;
+                        case 'Rain':
+                            icono.src = 'img/rainy-7.svg'
+                            console.log('LLUVIA');
+                            break;
+                        case 'Snow':
+                            icono.src = 'img/snowy-6.svg'
+                            console.log('NIEVE');
+                            break;
+                        case 'Clear':
+                            icono.src = 'img/day.svg'
+                            console.log('LIMPIO');
+                            break;
+                        case 'Atmosphere':
+                            icono.src = 'img/weather.svg'
+                            console.log('ATMOSFERA');
+                            break;
+                        case 'Clouds':
+                            icono.src = 'img/cloudy-day-1.svg'
+                            console.log('NUBES');
+                            break;
+                        default:
+                            icono.src = 'img/cloudy-day-1.svg'
+                            console.log('por defecto');
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        })
+    }
+
+})
